fb7733b0e9f159f7ddb840fa8c8a56f95060571c
JefriHarisLaksmana <jefriharis35@gmail.com>
2020-12-28 12:40:38 +0700

    Jefri & Bagus

--- app/src/main/java/com/android/project_jnb/CRUD/AddUserFragment.kt
+++ app/src/main/java/com/android/project_jnb/CRUD/AddUserFragment.kt
@@ -1,8 +1,11 @@
 import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
 import com.android.project_jnb.HomeActivity
 
 
@@ -10,7 +13,7 @@ class AddUserFragment : Fragment() {
     @Nullable
     fun onCreateView(inflater: LayoutInflater, @Nullable container: ViewGroup?, @Nullable savedInstanceState: Bundle?): View {
         val fragmentAddUserBinding: FragmentAddUserBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_user, container, false)
-        fragmentAddUserBinding.btnSubmit.setOnClickListener(object : OnClickListener() {
+        fragmentAddUserBinding.btnSubmit.setOnClickListener(object : View.OnClickListener() {
             fun onClick(v: View?) {
                 val id: Int = fragmentAddUserBinding.id.getText().toString().toInt()
                 val name: String = fragmentAddUserBinding.name.getText().toString()
@@ -19,7 +22,7 @@ class AddUserFragment : Fragment() {
                 user.setId(id)
                 user.setName(name)
                 user.setEmail(email)
-                HomeActivity.PeminjamRoomDatabase.myDao().addUser(user)
+                HomeActivity.PeminjamRoomDatabase.PeminjamDao().addUser(user)
                 Toast.makeText(getContext(), "User added successfully", Toast.LENGTH_SHORT).show()
             }
         })
